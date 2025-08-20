40d334f6e19bf558fd76359e9ead7393fc4fc699
FinallyThink <997854977@qq.com>
2025-08-20 08:24:18 +0800

    global Style

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import "./Home.less"
 function Home() {
   const navigate = useNavigate();
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./core/router.jsx"; // 引入路由配置
-import "./index.css";
+import "./global.less"; // 全局样式
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
